bf72091b9068d18e1d75d1b764161aff15bc384a
ghostmodd <ghost1mod@gmail.com>
2023-09-13 14:06:33 +0300

    refactor: change text layout in main page

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -6,14 +6,16 @@ function Techs() {
   return (
     <Section heading="Технологии" backgroundColor="gray">
       <article className="techs">
-        <h3 className="techs__heading">7 технологий</h3>
-        <p className="techs__text">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+        <h3 className="techs__heading">9 технологий</h3>
+        <p className="techs__text">За время обучения я освоил 9 технологий, которые применил в своих проектах.</p>
 
         <ul className="tag-list">
           <li className="tag-list__item">HTML</li>
           <li className="tag-list__item">CSS</li>
-          <li className="tag-list__item">JS</li>
+          <li className="tag-list__item">JavaScript</li>
+          <li className="tag-list__item">TypeScript</li>
           <li className="tag-list__item">React</li>
+          <li className="tag-list__item">Redux</li>
           <li className="tag-list__item">Git</li>
           <li className="tag-list__item">Express.js</li>
           <li className="tag-list__item">mongoDB</li>
